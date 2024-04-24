66a9e8318b816de8cfe9f3f60fe64f82c810e847
m4dde <macaa015@osloskolen.no>
2024-04-24 11:24:41 +0200

    js

---
+++ script.js
@@ -0,0 +1,20 @@
+/*const sections = document.querySelectorAll("#section");
+window.addEventListener("scroll", navHighlighter);
+function navHighlighter() {
+    let scrollY = window.scrollY
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+
+        const sectionTop = (current.getBoundingClientRect().top + window.scrollY) - 50;
+        sectionId = current.Attribute("section"); 
+
+        if (scrollY > sectionTop && scrollY <= sectionTop) + sectionHeight) {
+        document.querySelector(".navigation a[href*=" + sectionId + "]")
+        .classList.add("active");
+        } else {
+            document.querySelector(".navigation a[href*=" + sectionId + "]")
+            .classList.remove("active");
+        }
+    });
+}*/
+
