b1dd42ef4fe743c5e7f17a9be973ce2d02a55f34
m4dde <macaa015@osloskolen.no>
2024-04-24 13:12:46 +0200

    #

--- script.js
+++ script.js
@@ -1,42 +1,22 @@
-/*const sections = document.querySelectorAll("#section");
-window.addEventListener("scroll", navHighlighter);
-function navHighlighter() {
-    let scrollY = window.scrollYOffset
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight;
-
-        const sectionTop = (current.getBoundingClientRect().top + window.scrollY) - 50;
-        sectionId = current.Attribute("section"); 
-
-    if (scrollY > sectionTop && scrollY <= sectionTop) + sectionHeight) {
-    document.querySelector(".navigation a[href*=" + sectionId + "]").classList.add("active");
-     } else {
-        document.querySelector(".navigation a[href*=" + sectionId + "]").classList.remove("active");
-     }
-    });
-}*/
-
-const sections = document.querySelectorAll("section[id]");
-
+const sections = document.querySelectorAll("section");
+const sectionId = current.getAttribute("id");
 window.addEventListener("scroll", navHighlighter);
 
 function navHighlighter() {
-  
-  let scrollY = window.scrollYOffset;
-  
+  let scrollY = window.scrollY;
   sections.forEach(current => {
     const sectionHeight = current.offsetHeight;
 
-    const sectionTop = (current.getBoundingClientRect().top + window.scrollYOffset) - 50;
+    const sectionTop = (current.getBoundingClientRect().top + window.scrollY) - 50;
     sectionId = current.getAttribute("id");
     
     if (
       scrollY > sectionTop &&
       scrollY <= sectionTop + sectionHeight
     ){
-      document.querySelector(".navigation a[href*=" + sections + "]").classList.add("active");
+      document.querySelector(".navigation a[href='#" + sectionId + "']").classList.add("active");
     } else {
-      document.querySelector(".navigation a[href*=" + sections + "]").classList.remove("active");
+      document.querySelector(".navigation a[href='#" + sectionId + "']").classList.remove("active");
     }
   });
 }
